db1f356e7135cc18d7fff594e1b6f7af58d8f2c7
柏翰 黃 <asd1115056@gmail.com>
2019-07-30 13:56:35 +0800

    new env

--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
-"""
-Definition of views.
-"""
-
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.core.serializers.json import DjangoJSONEncoder
+from django.http import JsonResponse
+import json
 
+@login_required(login_url='/login/')
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
@@ -14,32 +17,6 @@ def home(request):
         'app/index.html',
         {
             'title':'Home Page',
-            'year':datetime.now().year,
-        }
-    )
-
-def contact(request):
-    """Renders the contact page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/contact.html',
-        {
-            'title':'Contact',
-            'message':'Your contact page.',
-            'year':datetime.now().year,
-        }
-    )
-
-def about(request):
-    """Renders the about page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/about.html',
-        {
-            'title':'About',
-            'message':'Your application description page.',
             'year':datetime.now().year,
         }
     )
